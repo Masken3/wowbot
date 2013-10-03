34044704cf65135c8a09e7a936b9be2b40ae7f1b
Masken3 <masken3@gmail.com>
2013-10-03 13:24:44 +0200

    Gui test: Size enforcement.

--- src/lua/gui/test.lua
+++ src/lua/gui/test.lua
@@ -1,18 +1,5 @@
 require "vcl"
 
-mainForm = VCL.Form{
-	name="mainForm",
-	caption = "Talents",
-	position="podesktopcenter",
-	height=400,
-	width=(256+64)*3,
-	onclosequery = "onCloseQueryEventHandler"
-}
-
-function onCloseQueryEventHandler(Sender)
-	return true -- the form can be closed
-end
-
 -- internalName:tab
 local tabs = {}
 
@@ -21,44 +8,72 @@ local resizingImages = {}
 
 local images = {}
 
+local iconSize = 40
+local marginFraction = 0.25
+
+local tabWidth = iconSize*4 + (iconSize*marginFraction*6)
+local tabHeight = iconSize*8 + (iconSize*marginFraction*10)
+
+mainForm = VCL.Form{
+	name="mainForm",
+	caption = "Talents",
+	position="podesktopcenter",
+	height=tabHeight,
+	width=tabWidth*3,
+	color=VCL.clBlack,
+	onclosequery = "onCloseQueryEventHandler"
+}
+
+function onCloseQueryEventHandler(Sender)
+	return true -- the form can be closed
+end
+
 function onResize(i)
 	--print(i.Width, i.Name)
 	--print(dump(i))
 	resizingImages[i.Name] = nil
 	if(not next(resizingImages)) then
-		setBackgroundPosition()
 		resizingImages['foobar'] = true -- prevent further resize
+		--setBackgroundPosition()
 	end
 	local t = images[i.Name]
 	if(t.talent) then
 		local tab = t.tab
-		print(tab.tabPage.." "..t.talent.col.." "..t.talent.row)
-		t.i.Left = tab.tabPage * (images[tab.internalName..'TopLeft'].i.Width +
-			images[tab.internalName..'TopRight'].i.Width) + t.talent.col * t.i.Width * 1.1 + t.i.Width * 0.1
-		t.i.Top = t.talent.row * t.i.Height * 1.1 + t.i.Height * 0.1
+		--print(tab.tabPage.." "..t.talent.col.." "..t.talent.row)
+		t.i.Left = tab.tabPage * tabWidth +
+			t.talent.col * t.i.Width * (1+marginFraction) +
+			t.i.Width * marginFraction
+		t.i.Top = (t.talent.row+1) * t.i.Height * (1+marginFraction) + t.i.Height * marginFraction
 	end
 end
 
-function setBackgroundPosition()
-	print("setBackgroundPosition")
-	for name,t in pairs(images) do
-		local tab = t.tab
-		if(t.part == 'TopLeft') then
-			t.i.Left = tab.tabPage * (t.i.Width + images[tab.internalName..'TopRight'].i.Width)
-		end
-		if(t.part == 'TopRight') then
-			local tlw = images[tab.internalName..'TopLeft'].i.Width
-			t.i.Left = tab.tabPage * (t.i.Width + tlw) + tlw
-		end
-		if(t.part == 'BottomRight') then
-			local tlw = images[tab.internalName..'TopLeft'].i.Width
-			t.i.Left = tab.tabPage * (t.i.Width + tlw) + tlw
-			t.i.Top = images[tab.internalName..'TopRight'].i.Height
-		end
-		if(t.part == 'BottomLeft') then
-			t.i.Left = tab.tabPage * (t.i.Width + images[tab.internalName..'TopRight'].i.Width)
-			t.i.Top = images[tab.internalName..'TopLeft'].i.Height
-		end
+function setBackgroundPosition(t)
+	local tab = t.tab
+	if(t.part == 'TopLeft') then
+		t.i.Left = tab.tabPage * tabWidth
+		t.i.Top = iconSize*(1+marginFraction)
+		-- left width is 4x of right width.
+		-- top height is 2x of bottom height.
+		t.i.Width = tabWidth * 4/5
+		t.i.Height = tabHeight * 2/3
+	end
+	if(t.part == 'TopRight') then
+		t.i.Left = tab.tabPage * tabWidth + tabWidth * 4/5
+		t.i.Top = iconSize*(1+marginFraction)
+		t.i.Width = tabWidth /5
+		t.i.Height = tabHeight * 2/3
+	end
+	if(t.part == 'BottomRight') then
+		t.i.Left = tab.tabPage * tabWidth + tabWidth * 4/5
+		t.i.Top = tabHeight * 2/3 + iconSize*(1+marginFraction)
+		t.i.Width = tabWidth /5
+		t.i.Height = tabHeight /3
+	end
+	if(t.part == 'BottomLeft') then
+		t.i.Left = tab.tabPage * tabWidth
+		t.i.Top = tabHeight * 2/3 + iconSize*(1+marginFraction)
+		t.i.Width = tabWidth * 4/5
+		t.i.Height = tabHeight /3
 	end
 end
 
@@ -68,16 +83,34 @@ for tab in cTalentTabs() do
 	--print(dump(tt))
 	if(tab.spellIcon == 11) then tab.tabPage = 1; end	-- patch MageFire
 	if(tab.classMask == 128) then	-- mage
-		-- gotta wait until onResize before positioning.
+		-- gotta wait until onResize before AutoSize takes effect.
+		-- tab background
 		for i,p in ipairs(backgroundParts) do
 			local n = tab.internalName..p
 			local i = VCL.Image{onresize="onResize", Name=n}
 			local t = {tab=tab, part=p, i=i}
 			images[n] = t
 			resizingImages[n] = true
-			i.AutoSize = true
+			i.AutoSize = false
+			i.Stretch = true
+			i.Proportional = false
+			setBackgroundPosition(t)
 			i:LoadFromFile(cIconRaw("Interface\\TalentFrame\\"..tab.internalName.."-"..p))
 		end
+		-- tab icon
+		do
+			local i = VCL.Image{}
+			i.Width = iconSize
+			i.Height = iconSize
+			i.Top = iconSize * marginFraction/2
+			i.Left = tabWidth * (tab.tabPage + 0.5) - (iconSize/2)
+			i.Hint = tab.name
+			i.ShowHint = true
+			i.Stretch = true
+			i.Proportional = true
+			i:LoadFromFile(cIconRaw(cSpellIcon(tab.spellIcon).icon))
+		end
+		-- talent icons
 		for talent in cTalents() do
 			if(talent.tabId == tab.id) then
 				local n = 's'..tostring(talent.id)
@@ -89,8 +122,8 @@ for tab in cTalentTabs() do
 				--resizingImages[n] = true
 				images[n] = {tab=tab, spell=s, i=i, talent=talent}
 				--i.AutoSize = true
-				i.Width = 40
-				i.Height = 40
+				i.Width = iconSize
+				i.Height = iconSize
 				i.Hint = s.name
 				i.ShowHint = true
 				i.Stretch = true
